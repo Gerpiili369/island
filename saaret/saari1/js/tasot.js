565bf50f6702d1f99528a7821dab06f36fe05316
Gerpiili369 <elektromiikka@hotmail.com>
2017-05-16 15:24:42 +0300

    tasot.js no longer looks shit

--- saaret/saari1/js/tasot.js
+++ saaret/saari1/js/tasot.js
@@ -21,17 +21,19 @@ function lataaTasot() {
                         [1,0,0,0,0,0,0,0,0,0,0,1,1,1],
                         [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
                     ],
-           "alkurivi":5,
-           "alkusarake":6,
-           "loppupaikat":[
+            "alkurivi":5,
+            "alkusarake":6,
+            "loppupaikat":[
                {"rivi":3,"sarake":13,"seuraavataso":1,"alkurivi":3,"alkusarake":0}
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "esineet":[
-           ],
-           "vastustajat":[
-           ]
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "esineet":[
+            ],
+            "avaimet":[
+            ],
+            "vastustajat":[
+            ]
         },
         {
             "nimi":"huone 2",
@@ -50,26 +52,28 @@ function lataaTasot() {
                         [1,0,0,0,0,1,0,0,0,0,0,1,0,1],
                         [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
                     ],
-           "loppupaikat":[
+            "loppupaikat":[
                {"rivi":5,"sarake":13,"seuraavataso":2,"alkurivi":5,"alkusarake":0},
                {"rivi":3,"sarake":0,"seuraavataso":0,"alkurivi":3,"alkusarake":13}
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "sumu":{"nimi":"sumu", "leveys":2,"pituus":2},
-           "esineet":[
-           ],
-           "vastustajat":[
-               {"nimi":"vastustaja","alkurivi":3,"alkusarake":12,"suunta":"ylös"},
-               {"nimi":"vastustaja","alkurivi":8,"alkusarake":2,"suunta":"ylös"}
-           ]
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "sumu":{"nimi":"sumu", "leveys":2,"pituus":2},
+            "esineet":[
+            ],
+            "avaimet":[
+            ],
+            "vastustajat":[
+                {"nimi":"vastustaja","alkurivi":3,"alkusarake":12,"suunta":"ylös"},
+                {"nimi":"vastustaja","alkurivi":8,"alkusarake":2,"suunta":"ylös"}
+            ]
         },
         {
             "nimi":"huone 3",
             "riviLkm":11,
             "sarakeLkm":14,
             "kartta":[
-                        [1,1,1,1,1,1,0,0,1,1,1,1,1,1], //rivi 0
+                        [1,1,1,1,1,1,1,1,1,1,1,1,1,1], //rivi 0
                         [1,0,0,0,0,1,0,0,1,0,0,0,0,1], //rivi 1
                         [1,0,1,1,0,1,0,0,1,0,1,1,0,1],
                         [1,0,0,1,0,0,0,0,0,0,0,0,0,1],
@@ -81,18 +85,20 @@ function lataaTasot() {
                         [1,0,0,0,0,1,0,1,0,0,0,0,0,1],
                         [1,1,0,1,1,1,1,1,1,1,1,1,1,1]
                     ],
-           "loppupaikat":[
+            "loppupaikat":[
                {"rivi":0,"sarake":6,"seuraavataso":7},
                {"rivi":0,"sarake":7,"seuraavataso":7},
                {"rivi":10,"sarake":2,"seuraavataso":3,"alkurivi":0,"alkusarake":2},
                {"rivi":5,"sarake":0,"seuraavataso":1,"alkurivi":5,"alkusarake":13}
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "esineet":[
-           ],
-           "vastustajat":[
-           ]   
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "esineet":[
+            ],
+            "avaimet":[
+            ],
+            "vastustajat":[
+            ]   
         },
         {
             "nimi":"huone 4",
@@ -111,15 +117,15 @@ function lataaTasot() {
                         [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
                         [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
                     ],
-             "loppupaikat":[
+            "loppupaikat":[
                {"rivi":0,"sarake":2,"seuraavataso":2,"alkurivi":10,"alkusarake":2},
                {"rivi":5,"sarake":13,"seuraavataso":4,"alkurivi":5,"alkusarake":0},
                {"rivi":5,"sarake":0,"seuraavataso":5,"alkurivi":4,"alkusarake":13}
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "esineet":[
-           ],
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "esineet":[
+            ],
             "vastustajat":[
                {"nimi":"vastustaja","alkurivi":5,"alkusarake":6,"suunta":"alas"}
            ]
@@ -141,14 +147,16 @@ function lataaTasot() {
                         [1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
                         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
                     ],
-             "loppupaikat":[
+            "loppupaikat":[
                {"rivi":5,"sarake":0,"seuraavataso":3,"alkurivi":5,"alkusarake":13},
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "esineet":[
-               {"nimi":"avaimet","rivi":9,"sarake":14,"pisteet":10}
-           ],
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "esineet":[                
+            ],
+            "avaimet":[
+                {"nimi":"avain","rivi":9,"sarake":14,"pisteet":10, "avaaRivi":0, "avaaSarake":6, "arvo":0, "taso":"huone 3"}
+            ],
             "vastustajat":[
                 {"nimi":"vastustaja","alkurivi":8,"alkusarake":2,"suunta":"oikea"},
                 {"nimi":"vastustaja","alkurivi":4,"alkusarake":13,"suunta":"ylös"},
@@ -172,19 +180,21 @@ function lataaTasot() {
                         [1,0,0,0,0,0,0,0,1,0,0,0,0,1],
                         [1,1,1,1,1,1,1,1,1,0,1,1,1,1]
                     ],
-             "loppupaikat":[
-               {"rivi":4,"sarake":13,"seuraavataso":3,"alkurivi":5,"alkusarake":0},
-               {"rivi":10,"sarake":9,"seuraavataso":6,"alkurivi":0,"alkusarake":9},
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "esineet":[
-               {"nimi":"avaimet","rivi":3,"sarake":3,"pisteet":10}
-           ],
+            "loppupaikat":[
+                {"rivi":4,"sarake":13,"seuraavataso":3,"alkurivi":5,"alkusarake":0},
+                {"rivi":10,"sarake":9,"seuraavataso":6,"alkurivi":0,"alkusarake":9}
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "esineet":[
+            ],
+            "avaimet":[
+                {"nimi":"avain","rivi":3,"sarake":3,"pisteet":10, "avaaRivi":5, "avaaSarake":13, "arvo":0, "taso":"huone 4"}
+            ],
             "vastustajat":[
                 {"nimi":"vastustaja","alkurivi":1,"alkusarake":7,"suunta":"vasen"},
                 {"nimi":"vastustaja","alkurivi":9,"alkusarake":5,"suunta":"oikea"}
-           ]
+            ]
         },
         {
             "nimi":"huone 7",
@@ -203,15 +213,15 @@ function lataaTasot() {
                         [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
                         [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
                     ],
-             "loppupaikat":[
-               {"rivi":0,"sarake":9,"seuraavataso":5,"alkurivi":10,"alkusarake":9},
-           ],
-           "karttakuvat":["tie","seinä"],
-           "pelaajanKulkuSallittuMax":0,
-           "esineet":[
-           ],
+            "loppupaikat":[
+                {"rivi":0,"sarake":9,"seuraavataso":5,"alkurivi":10,"alkusarake":9}
+            ],
+            "karttakuvat":["tie","seinä"],
+            "pelaajanKulkuSallittuMax":0,
+            "esineet":[
+            ],
             "vastustajat":[
-           ]
+            ]
         }
     ];
 }
